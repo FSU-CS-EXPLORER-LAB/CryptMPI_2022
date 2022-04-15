5b925dec14635235084f646767db5f8b3675a863
Mohsen <43177872+gavahi@users.noreply.github.com>
2022-04-15 10:38:12 -0400

    Delete MPICH/cryptMPI-mpich-3.3 directory

--- MPICH/cryptMPI-mpich-3.3/src/pm/hydra2/libhydra/mem/Makefile.mk
+++
@@ -1,9 +0,0 @@
-## -*- Mode: Makefile; -*-
-##
-## (C) 2017 by Argonne National Laboratory.
-##     See COPYRIGHT in top-level directory.
-##
-
-AM_CPPFLAGS += -I$(top_srcdir)/libhydra/mem
-
-noinst_HEADERS += libhydra/mem/hydra_mem.h
